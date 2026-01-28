b970ee62cd201be230b65cf37bf5cfd02627065a
ЕЕнот / RRaccoon <eenot138@gmail.com>
2026-01-28 18:04:37 +0200

    eelibs update

--- home/eelibs/import.js
+++ home/eelibs/import.js
@@ -4,3 +4,6 @@ import './js/notification.js';
 import './js/pages.js';
 import './js/translator.js';
 import './js/websocket.js';
+import './js/nav.js';
+import './js/settings.js';
+import './js/mobile.js';
